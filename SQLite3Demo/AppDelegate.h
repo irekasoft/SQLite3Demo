f1b0cf9b2133cba18a5a0b96aec94136b9f16dd7
Muhammad Hijazi <irekasoft@gmail.com>
2015-06-02 16:34:23 +0800

    Initial Commit

---
+++ SQLite3Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLite3Demo
+//
+//  Created by Hijazi on 2/6/15.
+//  Copyright (c) 2015 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQLite3Demo/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  SQLite3Demo
+//
+//  Created by Hijazi on 2/6/15.
+//  Copyright (c) 2015 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
